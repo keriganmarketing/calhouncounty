97ba75251312cf9b12a977c3fa6f0f8253e6e947
ToniSHernandez <toni@kerigan.com>
2021-04-22 07:44:48 -0500

    Video moved from WP content section row div to independent.

--- public/themes/wordplate/views/pages/front.blade.php
+++ public/themes/wordplate/views/pages/front.blade.php
@@ -18,21 +18,30 @@
             <div class="container">
 
                 <div class="row py-4 align-items-center">
-                    <div class="col-lg-6 py-4">
+                    <div class="col-lg-12 py-4">
                         <article class="front" tabindex="0">
                             
                             {{ the_content() }}
 
                         </article>
                     </div>
-                    <div class="col-lg-6 py-4" tabindex="0">
+                    {{-- <div class="col-lg-6 py-4" tabindex="0">
                         <div class="embed-responsive embed-responsive-16by9">
                         {!! $video !!}
                         <button v-if="!videoPlaying" class="video-button" aria-hidden="true" @click="playVideo" ref="videobutton"></button>
                         </div>
+                    </div> --}}
+                </div>
+                <div class="row py-4 align-items-center">
+                    <div class="col-lg-12 py-4">
+                        <div class="col-lg-12 py-4" tabindex="0">
+                            <div class="embed-responsive embed-responsive-16by9">
+                            {!! $video !!}
+                            <button v-if="!videoPlaying" class="video-button" aria-hidden="true" @click="playVideo" ref="videobutton"></button>
+                            </div>
+                        </div>
                     </div>
                 </div>
-
             </div>
         </main>
 
